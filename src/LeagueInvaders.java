a71d7c7c9718b7675124ab11188397d3a5353621
League Student <league@iMac-18.attlocal.net>
2019-06-26 16:26:42 -0700

    League Invaders
    :neutral_face:

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,23 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	JFrame frame;
+	GamePanel panel;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	public static void main(String[] args) {
+		LeagueInvaders make = new LeagueInvaders();
+		make.setup();
+	}
+	LeagueInvaders(){
+		panel=new GamePanel();
+		frame=new JFrame();
+	}
+	void setup() {
+		frame.add(panel);
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+	}
 }
